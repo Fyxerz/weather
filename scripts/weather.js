4271bf651faf4f0d8c3ae31988bf7a1a4bac3ae0
Pedro <pedro@tryacl.com>
2018-12-08 13:30:12 +0100

    Fix fetch, works now

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,10 @@
+const fetch = require('node-fetch')
+
 const KEY = '&APPID=afe3aa4dc7bf9cb1d8c8f1b622caf8d8'
 
-
-fetch('api.openweathermap.org/data/2.5/weather?q=Murcia' + KEY)
-  .then(response => console.log(response))
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Murcia' + KEY)
+  .then(response => response.json())
+  .then(json => console.log(json))
 
 
 
